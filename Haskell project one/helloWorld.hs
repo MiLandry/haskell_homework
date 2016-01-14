900298f8249ed27d5fae4a151e39899c921933c0
Landry <mlandry@cmg.int>
2016-01-14 09:48:22 -0500

    removed dumb file and more stuff

--- Haskell project one/helloWorld.hs
+++ Haskell project one/helloWorld.hs
@@ -5,3 +5,15 @@ hello = "Hello, World!"
 sumtorial :: Integer -> Integer
 sumtorial 0 = 0
 sumtorial n = n + sumtorial (n-1)
+
+
+isOdd :: Int -> Bool
+isOdd x = x `mod` 2 == 1
+
+whatGrade :: Int -> String
+whatGrade x
+  |x >= 90 = "A"
+  |x < 90 && x>=80 = "B"
+  |x < 80 && x>=72 = "C"
+  |x < 72 && x>=70 = "D"
+  |otherwise = "F" 
